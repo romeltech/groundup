a84a1e0a71eddfed291ab088a265f44ed73e2bcb
mel <indemnedev@gmail.com>
2019-09-30 21:41:36 +0400

    Laravel Ground Up E04 - Passing Data to Viewst

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Route::view('/', 'welcome');
 Route::view('/contact-us', 'contact');
 Route::view('/about', 'about');
+
+Route::get('customers', function(){
+    $customers = [
+        'John Doe',
+        'Jane Doe',
+        'Bob The Builder',
+    ];
+    return view('internals.customers', [
+        'customers' => $customers,
+    ]);
+});
