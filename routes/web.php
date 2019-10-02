0204f92ed412adc1cce5eb1c9fc892d838dc2c31
mel <indemnedev@gmail.com>
2019-10-02 21:11:11 +0400

    Laravel Ground Up E05 - Controllers

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,4 @@ Route::view('/', 'welcome');
 Route::view('/contact-us', 'contact');
 Route::view('/about', 'about');
 
-Route::get('customers', function(){
-    $customers = [
-        'John Doe',
-        'Jane Doe',
-        'Bob The Builder',
-    ];
-    return view('internals.customers', [
-        'customers' => $customers,
-    ]);
-});
+Route::get('customers', 'CustomersController@list');
