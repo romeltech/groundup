4b55e81e2c6b14aec0be649c8286687d95759ee9
mel <indemnedev@gmail.com>
2019-10-18 23:00:53 +0400

    Laravel Ground Up E040 - Image Upload: Cropping & Resizing - Part 2

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Company;
 use App\Customer;
-use App\Events\NewCustomerHasRegisteredEvent;
-use App\Mail\WelcomeNewUserMail;
 use Illuminate\Http\Request;
+use App\Mail\WelcomeNewUserMail;
 use Illuminate\Support\Facades\Mail;
+use Intervention\Image\Facades\Image;
+use App\Events\NewCustomerHasRegisteredEvent;
 
 class CustomersController extends Controller
 {
@@ -62,7 +63,7 @@ class CustomersController extends Controller
         $customer->update($this->validateRequest());
 
         $this->storeImage($customer);
-        
+
         return redirect('customers/'. $customer->id);
     }
 
@@ -79,21 +80,14 @@ class CustomersController extends Controller
     private function validateRequest()
     {
 
-        return tap( request()->validate([
+        return request()->validate([
             'name' => 'required|min:3',
             'email' => 'required|email',
             'active' => 'required',
             'company_id' => 'required',
+            'image' => 'sometimes|file|image|max:5000'
 
-        ]), function (){
-
-            if(request()->hasFile('image')){
-                request()->validate([
-                    'image' => 'file|image|max:5000'
-                ]);
-            }
-
-        });
+        ]);
     }
 
     private function storeImage($customer)
@@ -102,6 +96,11 @@ class CustomersController extends Controller
             $customer->update([
                 'image' => request()->image->store('uploads', 'public'),
             ]);
+
+            $image = Image::make( public_path('storage/' . $customer->image))->fit(300,300);
+            // $image = Image::make( public_path('storage/' . $customer->image))->fit(300,1000, null, 'top-left');
+            // $image = Image::make( public_path('storage/' . $customer->image))->crop(300,1000);
+            $image->save();
         }
     }
 }
